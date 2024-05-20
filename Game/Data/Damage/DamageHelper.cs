661309c7999627dc4c2add0a807864ad2a0b27c4
yanni <yanni@LAPTOP-0K20A8EE>
2024-05-20 20:16:36 +0200

    Rollchange Optimization

--- Game/Data/Damage/DamageHelper.cs
+++ Game/Data/Damage/DamageHelper.cs
@@ -1,6 +1,12 @@
 public static class DamageHelper {
     public static bool RollHitChange(short change) 
     {
+        if(change == 0) {
+            return false;
+        } else if( change >= 100) {
+            return true;
+        }
+
         Random random = new();
         return change > random.Next(0, 100);
     }

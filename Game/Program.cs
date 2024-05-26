91ba08dd879698525b3013e23e8256ef145dccfd
yanni <yanni@LAPTOP-0K20A8EE>
2024-05-26 21:09:25 +0200

    Added functionality to console game from latest changes

--- Game/Program.cs
+++ Game/Program.cs
@@ -13,19 +13,32 @@ city.AddStage(30);
 
 FireDefence fireDefence = new(7);
 BaseDefence baseDefence = new(40, fireDefence);
-Enemy enemy = new Mob(6000, baseDefence);
+Reward mobReward = new(coins: 500);
+Enemy enemy = new Mob(600, baseDefence, mobReward);
+Enemy enemy2 = new Mob(1200, baseDefence, mobReward);
+Enemy enemy3 = new Mob(1800, baseDefence, mobReward);
+Enemy enemy4 = new Mob(2000, baseDefence, mobReward);
+Enemy enemy5 = new Mob(2600, baseDefence, mobReward);
+Enemy enemy6 = new Mob(3200, baseDefence, mobReward);
+Enemy enemy7 = new Mob(6000, baseDefence, mobReward);
 
 WaveHelper.AddEnemyToStage(1, city, enemy);
+WaveHelper.AddEnemyToStage(1, city, enemy2);
+WaveHelper.AddEnemyToStage(1, city, enemy3);
+WaveHelper.AddEnemyToStage(1, city, enemy4);
+WaveHelper.AddEnemyToStage(1, city, enemy5);
+WaveHelper.AddEnemyToStage(1, city, enemy6);
+WaveHelper.AddEnemyToStage(1, city, enemy7);
 
 // skill
 SkillEffect skillEffect = new(5, 120);
 Skill skill = new CutSkill(10, 5, skillEffect);
 
 // chest
-Reward reward1 = new(equipment: new("10%", new()));
-Reward reward2 = new(equipment: new("20%", new()));
-Reward reward3 = new(equipment: new("30%", new()));
-Reward reward4 = new(equipment: new("40%", new()));
+Reward reward1 = new(coins: 100);
+Reward reward2 = new(coins: 200);
+Reward reward3 = new(coins: 300);
+Reward reward4 = new(coins: 400);
 
 
 List<ChestReward> chestRewards  = new List<ChestReward>();
@@ -35,12 +48,17 @@ chestRewards.Add(new ChestReward(reward3, 30));
 chestRewards.Add(new ChestReward(reward4, 40));
 Chest chest = new EquipmentChest(chestRewards);
 
+// upgrade
+
+BaseDamageUpgrade baseDamageUpgrade= new(5, 80, 1.1);
+
 while(!city.IsCityClear())
 {
     Stage activeStage = city.GetActiveStage();
     Enemy activeEnemy = activeStage.GetActiveEnemy();
 
     Console.WriteLine("Mob health: " + activeEnemy.health);
+    Console.WriteLine("Coins: " + character.currency.coins);
 
     string output = Console.ReadLine();
 
@@ -54,12 +72,31 @@ while(!city.IsCityClear())
             break;
         case "chest":
             Reward openReward = chest.OpenChest();
-            Console.WriteLine("Reward: " + openReward.equipment.name);
+            if(openReward.equipment != null) {
+                Console.WriteLine("Reward: " + openReward.equipment.name);
+            } else if(openReward.coins != null) {
+                character.AddCoins(openReward.coins ??= 0);
+                Console.WriteLine("Reward: " + openReward.coins + "Coins");
+            }
+            
+            break;
+        case "upgrade":
+            try 
+            {
+                baseDamageUpgrade.HandleUpgrade(character);
+                Console.WriteLine("Upgrade SucessFull!");
+            } 
+            catch (NoCurrencyException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
             break;
     }
 
     if(activeEnemy.IsDead())
     {
+        RewardHelper.GiveReward(character, activeEnemy.reward);
         activeStage.AdvanceStage();
 
         if(activeStage.IsStageClear())

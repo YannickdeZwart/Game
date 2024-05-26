600a7b5449d620fa6f6112b4a8841e0179c9fa1a
yanni <yanni@LAPTOP-0K20A8EE>
2024-05-26 21:07:12 +0200

    Added Currency class

--- Game/Data/Character/Character.cs
+++ Game/Data/Character/Character.cs
@@ -1,10 +1,17 @@
 public class Character {
     public BaseDamage damage;
     public Equipment sword;
+    public Currency currency;
 
     public Character(BaseDamage damage, Equipment sword)
     {
         this.damage = damage;
         this.sword = sword;
+        this.currency = new(0,0);
+    }
+
+    public void AddCoins(double coinAmount)
+    {
+        this.currency.coins += coinAmount;
     }
 }
---
+++ Game/Data/Currency/Currency.cs
@@ -0,0 +1,10 @@
+public class Currency {
+    public double coins;
+    public double diamonds;
+
+    public Currency(double coins, double diamonds) 
+    {
+        this.coins = coins;
+        this.diamonds = diamonds;
+    }
+}

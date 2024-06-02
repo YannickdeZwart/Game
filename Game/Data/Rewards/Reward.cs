281978547214f8295501a20686bb05963d27ba99
Yannick de Zwart <70627784+YannickdeZwart@users.noreply.github.com>
2024-06-02 23:45:01 +0200

    Added hero to rewards

--- Game/Data/Rewards/Reward.cs
+++ Game/Data/Rewards/Reward.cs
@@ -1,9 +1,11 @@
 public class Reward {
     public double? coins;
     public Equipment? equipment;
+    public Hero? hero;
 
-    public Reward(double? coins = null, Equipment? equipment = null) {
+    public Reward(double? coins = null, Equipment? equipment = null, Hero? hero = null) {
         this.coins = coins;
         this.equipment = equipment;
+        this.hero = hero;
     }
 }
